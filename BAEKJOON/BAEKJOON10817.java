98bedacefe492eaec93fb434a98a8b6101ec2b2e
aservmz@naver.com <aservmz@naver.com>
2020-07-12 04:18:58 +0900

    BAEKJOON10817

---
+++ BAEKJOON/BAEKJOON10817.java
@@ -0,0 +1,16 @@
+package BAEKJOON;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BAEKJOON10817 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int[] arr = new int[]{a,b,c};
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
