d95417dc1c1b57fe863bc33caf840cc61d58398a
aservmz@naver.com <aservmz@naver.com>
2020-08-01 17:31:18 +0900

    BAEKJOON2577

---
+++ BAEKJOON/BAEKJOON2577.java
@@ -0,0 +1,44 @@
+package BAEKJOON;
+
+import java.util.Scanner;
+
+public class BAEKJOON2577 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        int C = scan.nextInt();
+        int mul = A * B * C;
+
+        String[] mul_string = String.valueOf(mul).split("");
+
+        int[] number = new int[10];
+        for (int i = 0; i < number.length; i++) {
+            number[i] = i;
+        }
+
+        int[] count = new int[10];
+        for (int i = 0; i < mul_string.length; i++) {
+            for (int j = 0; j < number.length; j++) {
+                if (Integer.parseInt(mul_string[i]) == number[j]) {
+                    count[number[j]]++;
+                }
+            }
+        }
+
+
+        System.out.println(mul);
+
+        System.out.println("number");
+        for(int i=0; i<number.length; i++){
+            System.out.print(number[i]+" ");
+        }
+
+        System.out.println();
+        System.out.println("count");
+        for(int i=0; i<count.length; i++){
+            System.out.print(count[i]+" ");
+        }
+    }
+}
