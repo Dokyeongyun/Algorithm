10c4901055f0782489ea76808954ad8a00999dec
aservmz@naver.com <aservmz@naver.com>
2021-01-16 20:04:50 +0900

    BAEKJOON 2407 (BigInteger)

---
+++ BAEKJOON/BAEKJOON2407.java
@@ -0,0 +1,30 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BAEKJOON2407 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        BigInteger nFac = new BigInteger(String.valueOf(factorial(n)));
+        BigInteger mFac = new BigInteger(String.valueOf(factorial(m)));
+        BigInteger n_mFac = new BigInteger(String.valueOf(factorial(n-m)));
+
+        System.out.println(nFac.divide(mFac).divide(n_mFac));
+    }
+
+    static BigInteger factorial(int n){
+        if(n==1){
+            return BigInteger.valueOf(1);
+        }
+        return BigInteger.valueOf(n).multiply(factorial(n-1));
+    }
+}
