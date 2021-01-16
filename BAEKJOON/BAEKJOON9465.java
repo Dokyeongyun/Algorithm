df146b6e55d5bde0312d172b0cdbc4bf1e0d8bb3
aservmz@naver.com <aservmz@naver.com>
2021-01-16 21:11:29 +0900

    BAEKJOON 9465 (Dynamic Programming)

---
+++ BAEKJOON/BAEKJOON9465.java
@@ -0,0 +1,38 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BAEKJOON9465 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine());
+            int [][] arr = new int[2][n+1];
+            int [][] dp = new int[n+1][3];
+
+            for(int i=0; i<2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= n; j++){
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            dp[1][0] = 0; //X X
+            dp[1][1] = arr[0][1]; //O X
+            dp[1][2] = arr[1][1]; //X O
+
+            for(int i=2; i<=n; i++) {
+                dp[i][0] = Math.max(dp[i-1][0], Math.max(dp[i-1][1], dp[i-1][2]));
+                dp[i][1] = Math.max(dp[i-1][0], dp[i-1][2])+arr[0][i];
+                dp[i][2] = Math.max(dp[i-1][0], dp[i-1][1])+arr[1][i];
+            }
+
+            System.out.println(Math.max(dp[n][0], Math.max(dp[n][1], dp[n][2])));
+        }
+    }
+}
