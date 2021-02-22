39070f3575c16204f84241ce2c19314f719f461a
Dokyeongyun <aservmz@naver.com>
2021-02-22 18:35:49 +0900

    BAEKJOON 1138 (구현)

---
+++ BAEKJOON/BAEKJOON1138.java
@@ -0,0 +1,26 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BAEKJOON1138 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 1. 입력
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        // 2. 처리
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=N-1; i>=0; i--) list.add(arr[i], i+1);
+
+        // 3. 출력
+        for (Integer integer : list) System.out.print(integer + " ");
+    }
+}
