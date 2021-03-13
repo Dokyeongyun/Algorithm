4861f05d1b390769072b7e2901e6912445e7fcee
Dokyeongyun <aservmz@naver.com>
2021-03-13 16:21:38 +0900

    BAEKJOON 14425 (문자열) - Map을 이용한 풀이

--- BAEKJOON/BAEKJOON14425.java
+++ BAEKJOON/BAEKJOON14425.java
@@ -3,28 +3,43 @@ package BAEKJOON;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BAEKJOON14425 {
+    static int N, M;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
+        // List를 이용한 방법
+//        usingList();
+
+        // Map을 이용한 방법
+        usingMap();
+    }
+
+    static void usingMap() throws IOException{
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<N; i++){
+            map.put(br.readLine(), 1);
+        }
+        int count = 0;
+        for(int i=0; i<M; i++){
+            if(map.get(br.readLine()) != null) count++;
+        }
+        System.out.println(count);
+    }
+
+    static void usingList() throws IOException{
         String[] S = new String[N];
         for(int i=0; i<N; i++) S[i] = br.readLine();
-
         List<String> list = new ArrayList<>(Arrays.asList(S));
-
         int count = 0;
-
         for(int i=0; i<M; i++) if(list.contains(br.readLine())) count++;
-
         System.out.println(count);
     }
 }
