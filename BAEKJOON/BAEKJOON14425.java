89250b87f52ed99659c01a611cbc8560ebaa3444
Dokyeongyun <aservmz@naver.com>
2021-03-13 18:06:19 +0900

    BAEKJOON 14425 (문자열) - Trie를 이용한 풀이

--- BAEKJOON/BAEKJOON14425.java
+++ BAEKJOON/BAEKJOON14425.java
@@ -15,13 +15,55 @@ public class BAEKJOON14425 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // List를 이용한 방법
+         // List를 이용한 방법
 //        usingList();
 
-        // Map을 이용한 방법
-        usingMap();
+         // Map을 이용한 방법
+//        usingMap();
+
+
+        // Trie를 이용한 방법
+
+        // Trie 구성
+        Trie root = new Trie();
+        for(int i=0; i<N; i++){
+            String temp = br.readLine();
+
+            Trie curTrie = root;
+
+            for(int j=0; j<temp.length(); j++){
+                char c = temp.charAt(j);
+                if(curTrie.nextNode[c-'a'] == null) curTrie.nextNode[c-'a'] = new Trie();
+                curTrie = curTrie.nextNode[c-'a'];
+                if(j == temp.length()-1) curTrie.isFinished = true;
+            }
+        }
+
+        // Trie에서 찾기
+        int count = 0;
+        for(int i=0; i<M; i++){
+            String temp = br.readLine();
+
+            Trie curTrie = root;
+
+            for(int j=0; j<temp.length(); j++){
+                char c = temp.charAt(j);
+                if(curTrie.nextNode[c-'a'] == null) break;
+                curTrie = curTrie.nextNode[c-'a'];
+                if(j==temp.length()-1 && curTrie.isFinished) count++;
+            }
+        }
+        System.out.println(count);
     }
 
+    static class Trie {
+        Trie[] nextNode = new Trie[26];
+        boolean isFinished;
+
+        Trie() {}
+    }
+
+    // Map을 이용한 풀이 => 시간복잡도 O(1)
     static void usingMap() throws IOException{
         Map<String, Integer> map = new HashMap<>();
         for(int i=0; i<N; i++){
@@ -34,6 +76,7 @@ public class BAEKJOON14425 {
         System.out.println(count);
     }
 
+    // List를 이용한 풀이 => 시간복잡도 O(n)
     static void usingList() throws IOException{
         String[] S = new String[N];
         for(int i=0; i<N; i++) S[i] = br.readLine();
