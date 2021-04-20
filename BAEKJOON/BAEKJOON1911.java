5f562e493b668040b65ba91d81ff45bd44260bf4
Dokyeongyun <aservmz@naver.com>
2021-04-20 23:35:12 +0900

    [BAEKJOON 1911] 흙길 보수하기 (SILVER2, 스위핑)

---
+++ BAEKJOON/BAEKJOON1911.java
@@ -0,0 +1,49 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+https://www.acmicpc.net/problem/1911
+ */
+public class BAEKJOON1911 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(bf.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) return Integer.compare(o1[1], o2[1]);
+            return Integer.compare(o1[0], o2[0]);
+        });
+
+        int count = 0;
+        int range = 0;
+        for (int i = 0; i < N; i++) {
+            if (arr[i][0] > range){
+                range = arr[i][0];
+            }
+
+            if (arr[i][1] >= range) {
+                while (arr[i][1] > range) {
+                    range += L;
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
