7066dcdb056e56459d61c3d0e7ed9a0d6695c325
Dokyeongyun <aservmz@naver.com>
2021-06-18 22:25:29 +0900

    [BAEKJOON 10819] 차이를 최대로 (SILVER2, Backtracking)

---
+++ BAEKJOON/BAEKJOON10819.java
@@ -0,0 +1,54 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BAEKJOON10819 {
+    static int N;
+    static int[] arr;
+    static int[] arr2;
+    static int max = 0;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        arr2 = new int[N];
+        visit = new boolean[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        backtracking(0);
+        System.out.println(max);
+    }
+
+    static void backtracking(int count) {
+        if (count == N) {
+            int sum = 0;
+            for (int i = 0; i < N - 1; i++) {
+                sum += Math.abs(arr2[i] - arr2[i + 1]);
+            }
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visit[i]) {
+                continue;
+            }
+            visit[i] = true;
+            arr2[count] = arr[i];
+            backtracking(count + 1);
+            visit[i] = false;
+        }
+    }
+}
