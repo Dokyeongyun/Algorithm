c8a08e00d8dc7312591e5b2d69914b0f15cbdd52
Dokyeongyun <aservmz@naver.com>
2021-08-23 21:28:09 +0900

    [BAEKJOON 9655] 돌 게임

---
+++ BAEKJOON/BAEKJOON9655.java
@@ -0,0 +1,14 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BAEKJOON9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N % 2 == 1 ? "SK" : "CY");
+    }
+}
