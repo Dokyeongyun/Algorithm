29ac6dd54e6de75ee204ffef953b187491c37a2e
Dokyeongyun <aservmz@naver.com>
2021-09-12 14:40:29 +0900

    [BAEKJOON 11721] 열 개씩 끊어 출력하기

---
+++ BAEKJOON/BAEKJOON11721.java
@@ -0,0 +1,29 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/11721
+ */
+public class BAEKJOON11721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+
+        StringBuffer sb = new StringBuffer();
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (count == 10) {
+                count = 0;
+                sb.append("\n");
+            }
+            count++;
+            sb.append(input.charAt(i));
+        }
+
+        System.out.println(sb);
+    }
+}
