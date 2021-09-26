a2710e25b72dccae1d0d114d204677d446fc0604
Dokyeongyun <aservmz@naver.com>
2021-09-26 18:22:53 +0900

    [BAEKJOON 2789] 유학 금지

---
+++ BAEKJOON/BAEKJOON2789.java
@@ -0,0 +1,28 @@
+package BAEKJOON;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * https://www.acmicpc.net/problem/2789
+ */
+public class BAEKJOON2789 {
+    static final String CAMBRIDGE = "CAMBRIDGE";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String word = br.readLine();
+
+        StringBuffer answer = new StringBuffer();
+        for (int i = 0; i < word.length(); i++) {
+            int c = word.charAt(i);
+
+            if (c != 65 && c != 66 && c != 67 && c != 68 && c != 69 && c != 71 && c != 73 && c != 77 && c != 82) {
+                answer.append((char)c);
+            }
+        }
+        System.out.println(answer);
+    }
+}
