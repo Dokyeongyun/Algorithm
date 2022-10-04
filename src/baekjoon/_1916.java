dc15ca2157154b435653ec8b6c719fb6d15b7437
Dokyeongyun <aservmz@naver.com>
2022-10-04 16:14:41 +0900

    Beakjoon: 1916번 문제 [최소비용 구하기] (Java)

--- src/baekjoon/_1916.java
+++ src/baekjoon/_1916.java
@@ -5,84 +5,89 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/*
-https://www.acmicpc.net/problem/1916
+/**
+ * https://www.acmicpc.net/problem/1916
  */
 public class _1916 {
 
-    static ArrayList<Node>[] list;
-    static int[] minWeight;
+    static List<Node>[] adjacentNodeList;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int m = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
 
-        minWeight = new int[n+1];
-        for(int i=1; i<minWeight.length; i++){
-            minWeight[i] = Integer.MAX_VALUE;
-        }
-        list = new ArrayList[n+1];
-        for(int i=1; i<list.length; i++){
-            list[i] = new ArrayList<>();
+        adjacentNodeList = new ArrayList[N + 1];
+        for (int i = 1; i < adjacentNodeList.length; i++) {
+            adjacentNodeList[i] = new ArrayList<>();
         }
 
-        for(int i=0; i<m; i++){
+        for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int cur = Integer.parseInt(st.nextToken());
-            int next = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            list[cur].add(new Node(next, weight));
+            adjacentNodeList[start].add(new Node(end, cost));
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        dijkstra(start);
-        System.out.println(minWeight[end]);
+        long[] costs = dijkstra(N, start);
+        System.out.println(costs[end]);
     }
 
-    static void dijkstra(int start){
+    private static long[] dijkstra(int numberOfNodes, int start) {
+        boolean[] visit = new boolean[numberOfNodes + 1];
+        long[] costs = new long[numberOfNodes + 1];
+        Arrays.fill(costs, Integer.MAX_VALUE);
 
-        PriorityQueue<Integer> que = new PriorityQueue<>();
-        que.add(start);
-        minWeight[start] = 0;
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        queue.add(new Node(start, 0));
+        costs[start] = 0;
 
-        while(!que.isEmpty()){
-            int cur = que.poll();
+        while (!queue.isEmpty()) {
+            Node curNode = queue.poll();
 
-            for(int i=0; i<list[cur].size(); i++){
-                int next = list[cur].get(i).p;
-                int weight = list[cur].get(i).w;
+            if (visit[curNode.id]) {
+                continue;
+            }
 
-                if(minWeight[next] > minWeight[cur] + weight){
-                    minWeight[next] = minWeight[cur] + weight;
-                    que.add(next);
-                    System.out.println(que);
+            visit[curNode.id] = true;
+
+            for (Node nextNode : adjacentNodeList[curNode.id]) {
+                if (!visit[nextNode.id]) {
+                    if (costs[nextNode.id] >= costs[curNode.id] + nextNode.cost) {
+                        costs[nextNode.id] = costs[curNode.id] + nextNode.cost;
+                        queue.add(new Node(nextNode.id, costs[nextNode.id]));
+                    }
                 }
             }
         }
+
+        return costs;
     }
 
-    static class Node implements Comparable<Node>{
-        int p;
-        int w;
+    private static class Node implements Comparable<Node> {
+        int id;
+        long cost;
 
-        Node(int p, int w){
-            this.p = p;
-            this.w = w;
+        Node(int id, long cost) {
+            this.id = id;
+            this.cost = cost;
         }
 
         @Override
         public int compareTo(Node o) {
-            if(this.w - o.w < 0 ){
-                return 1;
-            }else{
-                return 0;
-            }
+            return Long.compare(this.cost, o.cost);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", id, cost);
         }
     }
 }
