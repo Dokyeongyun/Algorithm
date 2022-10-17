86017283deee9ee10f11d8d651a863dc089fb9cf
Dokyeongyun <aservmz@naver.com>
2022-10-17 23:28:27 +0900

    Beakjoon: 1647번 문제 [도시 분할 계획] (Java)

---
+++ src/baekjoon/_1647.java
@@ -0,0 +1,88 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/1647
+ */
+public class _1647 {
+
+    static List<Edge> edges;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        edges = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            edges.add(new Edge(start, end, weight));
+        }
+        Collections.sort(edges);
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        int costSum = 0;
+        int max = 0;
+        for (Edge edge : edges) {
+            if (find(edge.start) != find(edge.end)) {
+                costSum += edge.weight;
+                union(edge.start, edge.end);
+                max = edge.weight;
+            }
+        }
+
+        System.out.println(costSum - max);
+    }
+
+    static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int start;
+        int end;
+        int weight;
+
+        Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge e) {
+            return Integer.compare(weight, e.weight);
+        }
+    }
+}
