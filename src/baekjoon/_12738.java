8fd0326ead89cd8c8c6525aa4af71a47b4315342
Dokyeongyun <aservmz@naver.com>
2022-10-20 01:29:13 +0900

    Beakjoon: 12738번 문제 [가장 긴 증가하는 부분 수열 3] (Java)

---
+++ src/baekjoon/_12738.java
@@ -0,0 +1,57 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * https://www.acmicpc.net/problem/12738
+ */
+public class _12738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] numbers = new int[N];
+        int[] lis = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int lisLength = 1;
+        lis[0] = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (lis[lisLength-1] < numbers[i]) {
+                lis[lisLength] = numbers[i];
+                lisLength++;
+            } else {
+                int index = getLisIndex(0, lisLength - 1, numbers[i], lis);
+                lis[index] = numbers[i];
+            }
+        }
+
+        System.out.println(lisLength);
+    }
+
+    static int getLisIndex(int left, int right, int value, int[] lis) {
+        int mid;
+
+        while (left < right) {
+            mid = (left + right) / 2;
+
+            if (lis[mid] < value) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return right;
+    }
+}
